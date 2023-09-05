e10fd3d6815a326cfd3ba3ce8098bf9ce5833b48
Bijay-Gurung <stha4580@gmail.com>
2023-09-05 17:41:24 +0545

    My personal Portfolio website

---
+++ script.js
@@ -0,0 +1,11 @@
+let text = document.getElementById("changeText");
+let textList = ["Full-Stack Web Developer","Student","Python Developer","3D-Model Designer","Graphic-Designer","Story-writer","Video Grapher","Video Editor","Photo Grapher","Content Creator","Photo Editor"];
+let i = 0;
+
+function ChangingText() {
+    text.textContent = textList[i];
+    i = (i + 1) % textList.length;
+}
+
+ChangingText();
+setInterval(ChangingText, 2000);
